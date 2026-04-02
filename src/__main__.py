0ef60eca0ae682bf4caade2ca4212d178810ce48
Oleksandr Bachurin <obachuri@2-H-1.42heilbronn.de>
2026-04-02 20:01:23 +0200

    input file validation

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,12 @@
 import argparse
 import os
 import sys
+import json
+import torch
 
 import llm_sdk
 from llm_sdk import Small_LLM_Model
+from src.pd_valid import CFunctions, CFunction, CPrompt
 
 
 # from pydantic import BaseModel, Field, model_validator, field_validator
@@ -44,21 +47,125 @@ def main() -> None:
 
     print("-"*20)
     print(param)
+    print("-"*20)
+
+    funcs_def_json = ""
+    input_json = ""
+
+    try:
+        with open(param.functions_definition, "r") as defs:
+            funcs_def_json = json.load(defs)
+    except Exception as e:
+        print(f'Error reading file "{param.functions_definition}":', e)
+        sys.exit(1)
+
+    fn = CFunctions()
+    for f in funcs_def_json:
+        try:
+            funct = CFunction(**f)
+            fn.add(funct)
+        except Exception as e:
+            print('Error in function definition:', f, ':', e)
+
+    if (len(fn.fn) < 1):
+        print("Error - No functions defined! Check file:",
+              param.functions_definition)
+        sys.exit(1)
+    print("-"*20)
+
+    try:
+        with open(param.input, "r") as defs:
+            input_json = json.load(defs)
+    except Exception as e:
+        print(f'Error reading file "{param.input}":', e)
+        sys.exit(1)
+
+    promts: list[CPrompt] = []
+    for i in input_json:
+        try:
+            promts.append(CPrompt(**i))
+        except Exception as e:
+            print(f'Error in prompts "{i}":', e)
+
+    if (len(promts) < 1):
+        print("No prompts to process.")
+        sys.exit(0)
+
+    fun = ""
+    f_list = ""
+    max_function_name = 6
+    start_for_all = "fn_"
+    for f_ in fn.fn.values():
+        fun = fun + f'- {f_.name}: {f_.description} \n'
+        f_list = f_list + f'{f_.name}\n'
+        if (len(f_.name) > max_function_name):
+            max_function_name = len(f_.name)
+
+    print("max_function_name:", max_function_name)
 
     llm: Small_LLM_Model = llm_sdk.Small_LLM_Model()
 
-    aa = llm.encode("Hello! What is 2 plus 5?")
-    print(aa)
-
-    str_ = llm.decode([9707,    0, 3555,  374,  220,   17, 5519,  220,   20,   30])
-    print(str_)
- #   bb = llm.get_logits_from_input_ids([9707,    0, 3555,  374,  220,   17, 5519,  220,   20,   30])
     vocab_path = llm.get_path_to_vocab_file()
     merges_path = llm.get_path_to_merges_file()
     tokenizer_path = llm.get_path_to_tokenizer_file()
     print("vocab_path:", vocab_path)
     print("merges_path:", merges_path)
-    print("tokenizer path", tokenizer_path)
+    print("tokenizer_path:", tokenizer_path)
+
+    llm.max_new_tokens = max_function_name
+    llm.temperature = 0.0
+
+    for promt in promts:
+        str_promt = promt.prompt
+        print("-"*20)
+        # print(str_promt)
+        str_ = f"""<|im_start|>system
+You are a function selector.
+Choose one function name.
+
+Functions:
+{fun}
+
+Rules:
+- Output ONLY the function name
+- Do NOT output <think> </think>
+- Do NOT explain
+- If nothing matches, output fn_NONE
+<|im_end|>
+
+<|im_start|>user
+{str_promt}
+<|im_end|>
+
+<|im_start|>assistant
+"""
+        print(str_)
+        print("-"*10)
+        aa = llm.encode(str_)
+#        print(aa)
+
+        aa_1 = aa.flatten().tolist()
+
+        aa = llm.encode(start_for_all)
+        aa_1.extend(aa.flatten().tolist())
+
+        cc = llm.get_logits_from_input_ids(aa_1)
+#    print(type(cc))
+#    print(cc)
+
+        cc_t = torch.tensor(cc)
+        next_token = torch.argmax(cc_t).item()
+
+        aa_1.append(next_token)
+
+        for i in range(1, 20):
+            cc = llm.get_logits_from_input_ids(aa_1)
+            cc_t = torch.tensor(cc)
+            next_token = torch.argmax(cc_t).item()
+            aa_1.append(next_token)
+
+        str_ = llm.decode(aa_1)
+        print(str_)
 
 
 if __name__ == "__main__":
---
+++ src/pd_valid.py
@@ -0,0 +1,73 @@
+import re
+from pydantic import BaseModel, Field, field_validator
+# , ValidationError , model_validator
+
+
+class CParameterType(BaseModel):
+    """
+    Single parameter or return value type in a function definition.
+    """
+    type: str
+
+    @field_validator("type")
+    def validate_type(cls, v: str) -> str:
+        allowed_types = {"number", "string", "boolean", "integer",
+                         "float", "array", "object"}
+        if v not in allowed_types:
+            raise ValueError(
+                f"Unsupported type '{v}'."
+                " Type must be one of:", allowed_types)
+        return v
+
+
+class CFunction(BaseModel):
+    name: str = Field(min_length=1)
+    description: str = Field(min_length=5)
+    parameters: dict[str, CParameterType] = {}
+    returns: CParameterType
+
+    @field_validator("name")
+    def name_check(cls, v):
+        v = v.strip()
+        if len(v) < 1:
+            raise ValueError("Name of the function must not be empty!")
+        if not bool(re.match(r'^[A-Za-z][A-Za-z0-9_]*$', v)):
+            raise ValueError('Function name must begin from a letter and '
+                             'contain only letters, numbers, or "_".'
+                             f"(name={v})")
+        return v
+
+    @field_validator("description")
+    def name_description(cls, v):
+        v = v.strip()
+        if len(v) < 5:
+            raise ValueError("Description of the function must not be empty!")
+        return v
+
+    # @field_validator("returns")
+    # def returns_check(cls, v):
+    #     if (len(v) < 1):
+    #         raise ValueError("Returns value type of the"
+    #                          " function must be defined!")
+    #     return v
+
+
+class CFunctions(BaseModel):
+    fn: dict[str, CFunction] = {}
+
+    def add(self, f: CFunction):
+        if not (self.fn.get(f.name, None) is None):
+            raise ValueError("Name of the function must be unique!"
+                             f"(name='{f.name}')")
+        self.fn[f.name] = f
+
+
+class CPrompt(BaseModel):
+    prompt: str = Field(min_length=5)
+
+    @field_validator("prompt")
+    def validate_prompt(cls, v: str) -> str:
+        v = v.strip()
+        if (len(v) < 5):
+            raise ValueError("Prompt cannot be empty.")
+        return v
